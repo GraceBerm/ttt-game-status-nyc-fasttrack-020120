797f0b5082fd0b8e71792c47922f1f8bc621a097
Grace Berman <grace.g.berman@gmail.com>
2020-03-22 15:05:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,14 +16,15 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" &&
-    board[win_combination[1]] == "X" &&
-    board[win_combination[2]] == "X"
-    else
-    board[win_combination[0]] == "O" &&
-    board[win_combination[1]] == "O" &&
-    board[win_combination[2]] == "O"
-  end
+  WIN_COMBINATIONS.select do |win_combination|
+    if (board[win_combination[0]] == "X" &&
+       board[win_combination[1]] == "X" &&
+       board[win_combination[2]] == "X")
+       true
+    elsif (board[win_combination[0]] == "O" &&
+       board[win_combination[1]] == "O" &&
+       board[win_combination[2]] == "O")
+       true
+     end
   end
 end
